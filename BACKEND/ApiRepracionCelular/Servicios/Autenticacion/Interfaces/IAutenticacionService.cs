3ba494a02694acc57db0d1f2cac9c3929fd0721a
CristhianGuevara-code <guevara@gmail.com>
2024-08-17 10:17:16 -0600

    backend

---
+++ BACKEND/ApiRepracionCelular/Servicios/Autenticacion/Interfaces/IAutenticacionService.cs
@@ -0,0 +1,10 @@
+﻿using ApiRepracionCelular.Dto;
+using ApiRepracionCelular.Dto.Autenticacion;
+
+namespace ApiRepracionCelular.Servicios.Autenticacion.Interfaces
+{
+    public interface IAutenticacionService
+    {
+        public Task<UsuarioDto> IniciarSesion(LoginDatosDTO datos);
+    }
+}
